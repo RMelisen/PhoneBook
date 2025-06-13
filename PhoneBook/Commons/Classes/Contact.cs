0d766add40d1d284acf14bef528f80a7cd9c2da6
Romain Mélisen <r.melisen@outlook.com>
2025-06-13 09:03:51 +0200

    Initialized default folders and Contact class

---
+++ PhoneBook/Commons/Classes/Contact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Commons.Classes
+{
+    internal class Contact
+    {
+        #region Properties
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Contact(string name, string phoneNumber, string email, string address)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Address = address;
+        }
+
+        public Contact(int id, string name, string phoneNumber, string email, string address)
+            : this(name, phoneNumber, email, address)
+        {
+            Id = id;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return $"{Name} - {PhoneNumber} - {Email} - {Address}";
+        }
+    }
+}
